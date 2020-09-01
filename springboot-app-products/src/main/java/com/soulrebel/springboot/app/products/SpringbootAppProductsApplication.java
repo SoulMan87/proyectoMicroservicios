ada9934d74aa67b0c76a6832c005aa657a6c95de
Jonathan <jonathanhinestroza87@gmail.com>
2020-09-01 17:54:49 -0500

    eureka server eureka client

--- springboot-app-products/src/main/java/com/soulrebel/springboot/app/products/SpringbootAppProductsApplication.java
+++ springboot-app-products/src/main/java/com/soulrebel/springboot/app/products/SpringbootAppProductsApplication.java
@@ -2,7 +2,9 @@ package com.soulrebel.springboot.app.products;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class SpringbootAppProductsApplication {
 
