6a07eb2a32266af6eb4567e88f971009d4f19c48
jonathan <jonathanhinestroza@gmail.com>
2020-09-20 18:45:44 -0500

    item controller methods

--- springboot-service-item/src/main/java/com/soulrebel/springboot/app/item/models/controllers/ItemController.java
+++ springboot-service-item/src/main/java/com/soulrebel/springboot/app/item/models/controllers/ItemController.java
@@ -13,9 +13,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.List;
@@ -71,4 +69,23 @@ public class ItemController {
         }
         return new ResponseEntity<Map<String, String>>(jsonMap, HttpStatus.OK);
     }
+
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Optional<Product> create(@RequestBody Product product) {
+        return itemService.saveServ(product);
+    }
+
+    @PutMapping("edit/{id}")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Optional<Product> edit(@RequestBody Product product, @PathVariable Long id) {
+        return itemService.updateServ(product, id);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        itemService.deleteServ(id);
+    }
+
 }
