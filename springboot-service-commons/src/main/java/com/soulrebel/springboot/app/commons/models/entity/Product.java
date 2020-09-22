e7cfa8e31f61ec97878601ee6155dfff7a5266c2
jonathan <jonathanhinestroza@gmail.com>
2020-09-21 19:32:50 -0500

    build jar from commons

---
+++ springboot-service-commons/src/main/java/com/soulrebel/springboot/app/commons/models/entity/Product.java
@@ -0,0 +1,63 @@
+package com.soulrebel.springboot.app.commons.models.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "products")
+public class Product implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String price;
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createdAt;
+    @Transient
+    private Integer port;
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public void setPort(Integer port) {
+        this.port = port;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    private static final long serialVersionUID = 1L;
+}
---
+++ springboot-service-commons/src/test/java/com/soulrebel/springboot/app/commons/SpringbootServiceCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soulrebel.springboot.app.commons;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServiceCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
